0771594fb99ffc3d00ac1e673daaea718d3722ee
Youngju So <joo920427@gmail.com>
2022-05-09 09:23:16 +0900

    첫 커밋

---
+++ src/chap04/ChangeValue1.java
@@ -0,0 +1,18 @@
+package chap04;
+
+public class ChangeValue1 {
+    public static void main(String[] args) {
+        // 년도
+        int year = 2002;
+
+        System.out.print("한국 월드컵은 ");
+        System.out.print(year);
+        System.out.println("년도에 개최되었습니다.");
+
+        // 변수에 다른 값 할당
+        year = 2018;
+        System.out.print("동계올림픽이 ");
+        System.out.print(year);
+        System.out.println("년도에 개최되었습니다.");
+    }
+}
---
+++ src/chap04/Variable.java
@@ -0,0 +1,23 @@
+package chap04;
+
+public class Variable {
+    public static void main(String[] args) {
+
+        // 제목
+        String title;
+        title = "Java 입문";
+
+        //가격
+        int price;
+        price = 30000;
+
+        System.out.print("책제목 : ");
+        System.out.println(title);
+
+        System.out.println();
+
+        System.out.print("책가격 : ");
+        System.out.println(price);
+    }
+
+}
